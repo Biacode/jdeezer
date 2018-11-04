035bb06a91299cb93b88f50a5100ce37c6729284
Arthur Asatryan <biacoder@gmail.com>
2018-11-05 02:02:17 +0400

    Add album comments #13 - Initial stage

---
+++ src/main/kotlin/org/biacode/jdeezer/model/album/AlbumCommentsResponseModel.kt
@@ -0,0 +1,14 @@
+package org.biacode.jdeezer.model.album
+
+import org.biacode.jdeezer.model.common.response.ResponseModel
+
+/**
+ * Created by Arthur Asatryan.
+ * Date: 11/5/18
+ * Time: 1:41 AM
+ */
+data class AlbumCommentsResponseModel(
+        val data: List<AlbumCommentModel>,
+        val total: Int,
+        val next: String
+) : ResponseModel
---
+++ src/main/kotlin/org/biacode/jdeezer/model/album/request/AlbumCommentRequest.kt
@@ -0,0 +1,14 @@
+package org.biacode.jdeezer.model.album.request
+
+import org.biacode.jdeezer.model.common.request.AbstractJDeezerPaginationAwareRequest
+
+/**
+ * Created by Arthur Asatryan.
+ * Date: 11/5/18
+ * Time: 1:35 AM
+ */
+data class AlbumCommentRequest(
+        val albumId: Int,
+        override val index: Int = 0,
+        override val limit: Int = 10
+) : AbstractJDeezerPaginationAwareRequest(index, limit)
