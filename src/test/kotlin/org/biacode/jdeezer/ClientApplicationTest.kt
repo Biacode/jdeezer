1ae827aeece438a337020d32235746828bb8f151
Arthur Asatryan <biacoder@gmail.com>
2018-11-04 18:04:45 +0400

    #1 Initial commit and initial structure

---
+++ src/test/kotlin/org/biacode/jdeezer/ClientApplicationTest.kt
@@ -0,0 +1,17 @@
+package org.biacode.jdeezer
+
+import org.junit.Test
+
+/**
+ * Created by Arthur Asatryan.
+ * Date: 11/4/18
+ * Time: 5:55 PM
+ */
+class ClientApplicationTest {
+
+    @Test
+    fun `test initialize`() {
+
+    }
+
+}
